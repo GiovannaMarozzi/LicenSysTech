9d370b53994f7c94aa09daabb134ae12e9488ab9
Giovanna Marozzi <giovanna.machado99@hotmail.com>
2023-08-02 20:07:01 -0300

    Initializing project

---
+++ src/main/java/com/empresa/sankya/SankyaApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.sankya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SankyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SankyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/sankya/SankyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.sankya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SankyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
